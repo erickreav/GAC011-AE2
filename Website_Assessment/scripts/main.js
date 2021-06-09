a137acaf1914ca80ab3dca0bb37a25c4ff75847d
erickreav <83837046+erickreav@users.noreply.github.com>
2021-06-08 23:12:45 -0600

    Delete Website_Assessment directory

--- Website_Assessment/scripts/main.js
+++
@@ -1,8 +0,0 @@
-function newImage (){
-	var image = document.getElementById("myImage") ;
-	if(image.src.match("image2")){
-		image.src = "images/image1.jpg";
-		}else{
-		image.src = "images/image2.jpg"
-		}
-	}
